b2095861e2cd973d888da9c9222e39fa8489cb44
Lotfi <mlb20@nyu.edu>
2019-06-26 18:23:28 +0400

    take away the return value of the straxen version, it will be given in the pm.ini file

--- xom/backend/run_database.py
+++ xom/backend/run_database.py
@@ -37,38 +37,40 @@ def getKrCalibration(time_of_day='2019-01-01 00:00:00') :
     cursor = list( collection.find(query) ) # make a list of the results of the querry
 
     #get the version of pax    
-
+    if len(cursor):
     #print('the type of that observable before we get it to querry is: ', type(cursor[0]))
-    for i_c in cursor:
-        #print('the type of this i_c: ', type(i_c))
-        run_number = i_c['number']
-        run_name  = i_c['name']
-        #run_start = i_c['start']
-        #run_end   = i_c['end']
-        run_source = i_c['source']['type']
-        #print("the type of source", run_source)
-        if "data" in i_c.keys():
-            pax_version = "pax_%s" % i_c["data"][0]["pax_version"]
+        for i_c in cursor:
+            #print('the type of this i_c: ', type(i_c))
+            run_number = i_c['number']
+            run_name  = i_c['name']
+            #run_start = i_c['start']
+            #run_end   = i_c['end']
+            run_source = i_c['source']['type']
+            #print("the type of source", run_source)
+            #if "data" in i_c.keys():
+            #    pax_version = "pax_%s" % i_c["data"][0]["pax_version"]
 
-        TagFile = False
-        if 'tags' in i_c.keys() and len(i_c['tags']) > 0:            
-            for itagdict in i_c['tags']:
-                for v in itagdict.values():
-                    if (isinstance(v,str)) and (v in ["bad","messy","_blinded"]): 
-                        #print("this run must be bad: ", run_name, v)
-                        TagFile = True
-                        break
+            TagFile = False
+            if 'tags' in i_c.keys() and len(i_c['tags']) > 0:
+                for itagdict in i_c['tags']:
+                    for v in itagdict.values():
+                        if (isinstance(v,str)) and (v in ["bad","messy","_blinded"]):
+                            #print("this run must be bad: ", run_name, v)
+                            TagFile = True
+                            break
 
-        if not TagFile:
-            if run_source in ["Kr83m" , "Rn220"]:
-                data_type = "calibration"
-                run_source = "kr"
-            else:
-                data_type= "background"
-                run_source = " "
-            #run_dbtags.append((run_number, run_name, pax_version))
-    print(data_type, run_source, run_name, run_number, pax_version)
-    sys.exit(1)
+            if not TagFile:
+                if run_source in ["Kr83m" , "Rn220"]:
+                    data_type = "calibration"
+                    run_source = "kr"
+                else:
+                    data_type= "background"
+                    run_source = " "
+            #run_dbtags.append((run_number, run_name)
+        print(data_type, run_source, run_name, run_number)
+        sys.exit(1)
+    else:
+        sys.exi(0)
 
 if __name__ == "__main__":
     getKrCalibration(sys.argv[1])
