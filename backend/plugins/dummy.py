a4b9b3df77223293014b06afa9bafb1f07a34347
lmanenti <lauramanenti@gmail.com>
2020-11-25 02:56:11 -0600

    some (old) changes to dummy.py

--- backend/plugins/dummy.py
+++ backend/plugins/dummy.py
@@ -13,21 +13,24 @@ class MyPlugin(strax.Plugin):
     provides = ('fancy_peaks')
 
     data_kind = 'fancy_peaks'
-    depends_on = 'peak_basics'
+    #depends_on = 'peak_basics'
+    depends_on = 'peaklets'
     
     def infer_dtype(self):
         dtype = strax.time_fields + [(('Electron life time in TPC', 'elif'), np.float32)]
+        
         return dtype
     
-    def compute(self, peaks, start, end):
+    def compute(self, peaklets, start, end):
         res = np.ones(1, dtype=strax.time_fields + [(('Electron life time in TPC', 'elif'), np.float32)])
         res['time'] = start
         res['endtime'] = end
         option_value = self.config['peak_split_min_area']
-        res['elif'] = compute_elife(peaks, option_value)
+        res['elif'] = compute_elife(peaklets, option_value)
+        print("doing stuff")
         
         return res
     
-def compute_elife(peak_basics, option_value):
+def compute_elife(peaklets, option_value):
     return 100
     
